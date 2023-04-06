2738f6c2c5cf79869ae4eb8313ec7952b33d6402
Janusz Witkowski <janusz.witkowski@vewd.com>
2023-04-06 17:13:06 +0200

    Implement Bloom's Filter

--- z15_bloom/bloom.py
+++ z15_bloom/bloom.py
@@ -1,2 +1,27 @@
+#!/usr/bin/python3
 import mmh3
-import bitarray
+from bitarray import bitarray
+
+class BloomFilter:
+    def __init__(self, number_of_hashes, bitarray_length):
+        self.k = number_of_hashes
+        self.n = bitarray_length
+        self.hashes = [(lambda x: mmh3.hash(x, i ** 2, signed=False)) for i in range(number_of_hashes)]
+        self.array = bitarray(bitarray_length)
+        for i in range(bitarray_length):
+            self.array[i] = 0
+    
+    def add(self, x):
+        for i in range(self.k):
+            self.array[self.hashes[i](x) % self.n] = 1
+    
+    def check(self, x):
+        for i in range(self.k):
+            if self.array[self.hashes[i](x) % self.n] == 0:
+                return False
+        return True
+    
+    def clear(self):
+        for i in range(self.n):
+            self.array[i] = 0
+
