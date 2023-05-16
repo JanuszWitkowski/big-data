e338ff1e36699c17608fbe67f8db1dd26536e224
Janusz Witkowski <janusz.witkowski@vewd.com>
2023-05-16 12:11:55 +0200

    Cleanup code

--- minhash.py
+++ minhash.py
@@ -7,14 +7,10 @@ def minHash(L: List[Any], hs: List[Callable[[Any], int]]) -> List[int]:
 
 
 if __name__ == "__main__":
-    n = 11
-    m = 20
+    n = 10
+    m = 50
     s = "test"
     hs = [h for h in map(lambda i: lambda x: mmh3.hash(key=x, seed=i), [i**2 for i in range(n)])]
-    # print(type(hs))
-    # print([h(s) for h in hs])
-    # print(hs[1](s + s))
-    # print([mmh3.hash(s, i**2) for i in range(n)])
-    L = [bin(j**3) for j in range(20)]
+    L = [bin(j**3) for j in range(m)]
     min_hash = minHash(L, hs)
     print(min_hash)
