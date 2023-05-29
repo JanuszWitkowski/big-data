3cb86410d0a962700e04bd759e4036f2ed1d6c01
Janusz Witkowski <46355383+JanuszWitkowski@users.noreply.github.com>
2023-05-29 09:49:16 +0200

    Just try to see good results

--- jaccard.py
+++ jaccard.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 from minhash import minHash
-from kgram import min_k_gram_hash, k_gram_minHashes
+from kgram import k_grams, k_gram_minHashes
 from wordcloud import open_and_clean_file
 from utils import get_files, proper_name
 from typing import Any, List, Callable
 from numbers import Number
+
+def get_docs_k_grams(docs: List[str], k: int) -> List[List[list]]:
+    return [k_grams(doc, k) for doc in docs]
 
 def get_docs_minHashes(docs: List[list], hashes: List[Callable[[Any], int]], k: int) -> List[List[int]]:
     return [k_gram_minHashes(doc, hashes, k) for doc in docs]
@@ -14,13 +17,19 @@ def jaccard_estimate(minHashesA: List[int], minHashesB: List[int]) -> Number:
         return -1
     # print(f"{len([m for i, m in enumerate(minHashesA) if minHashesA[i] == minHashesB[i]])}, {len(minHashesA)}")
     return len([m for i, m in enumerate(minHashesA) if minHashesA[i] == minHashesB[i]]) / len(minHashesA)
-    # return float(len(set(minHashesA).intersection(minHashesB))) / float(len(set(minHashesA).union(minHashesB)))
 
 def concatenate_strings(strings: List[str]) -> str:
     res = ""
     for string in strings:
         res = res + string + " "
     return res
+
+def jaccard_theory(docA: List[list], docB: List[list]) -> Number:
+    # setA = set(docA)
+    # setB = set(docB)
+    setA = set([str(doc) for doc in docA])
+    setB = set([str(doc) for doc in docB])
+    return len(setA.intersection(setB)) / len(setA.union(setB))
 
 
 
@@ -43,18 +52,26 @@ if __name__ == "__main__":
     docs = [concatenate_strings(open_and_clean_file(DIR_NAME + filename, STOP_WORDS, False)) for filename in filenames]
     names = [proper_name(filename) for filename in filenames]
 
+    print("Getting k-grams...")
+    kgrams = get_docs_k_grams(docs, k)
+    # print(kgrams)
+
     print("Getting minHashes...")
     ms = get_docs_minHashes(docs, hashes[2], k)
     n = len(ms)
 
-    print("Calculating...")
-    jaccards = [jaccard_estimate(ms[i], ms[j]) for i in range(n) for j in range(i, n)]
     infos = [f"{names[i]} vs {names[j]}" for i in range(n) for j in range(i, n)]
+    print("Calculating empirical...")
+    empirum = [jaccard_estimate(ms[i], ms[j]) for i in range(n) for j in range(i, n)]
+    print("Calculating theoretical...")
+    theory = [jaccard_theory(kgrams[i], kgrams[j]) for i in range(n) for j in range(i, n)]
+    sanity_check = [jaccard_theory(docs[i], docs[j]) for i in range(n) for j in range(i, n)]
 
     print(names)
-    for i, _ in enumerate(jaccards):
-        # print(f"{infos[i]}: {jaccards[i]}")
-        print(f"{jaccards[i]} | {infos[i]}")
+    print("Comparison: empirical | theoretical | sanity-check")
+    for i, _ in enumerate(empirum):
+        # print(f"{infos[i]}: {empirum[i]}")
+        print(f"{infos[i]}:\t{empirum[i]} | {theory[i]} | {sanity_check[i]}")
     # print(docs)
     # print(names)
 
