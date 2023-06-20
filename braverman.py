d34ab74fd87a349def63c9bbb87c291a7609a131
Janusz Witkowski <46355383+JanuszWitkowski@users.noreply.github.com>
2023-06-20 16:50:32 +0200

    Need to make corrections to Braverman

--- braverman.py
+++ braverman.py
@@ -1,4 +1,74 @@
 #!/usr/bin/python3
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from math import sqrt
+from scipy.stats import chisquare
+
+class SlidingWindow:
+    def __init__(self, k: int):
+        self.k = k
+        self.n = 0
+        self.c = 0
+        self.elem = None
+        # self.sample = []
+        self.treewidth = "Bojko baza"
+    
+    def read(self, x):
+        self.n += 1
+        if random.random() < 1.0/self.n:
+        # if random.random() < 1.0/sqrt(self.n):
+        # if random.random() < 1.0/():
+            self.elem = x
+            self.c = self.n
+            # self.sample.append(self.c)
+            # self.sample.append(self.elem)
+        self.n %= self.k
+    
+    def get(self):
+        return self.elem, self.c
+    
+    # def get_sample(self):
+    #     return self.sample
+    
+    def clear(self):
+        self.n = 0
+        self.c = 0
+        self.elem = None
+        # self.sample = []
+
+
+def histogram(sample: list, n: int):
+    # plt.figure(figsize=(16, 8))
+    plt.hist(sample, density=True, bins=n)
+    plt.savefig("braverman/histogram.png")
+
+
+def check_correctness(n: int, k: int, iter: int):
+    sw = SlidingWindow(k)
+    data = [i for i in range(n)]
+    sample = []
+    for _ in range(iter):
+        for j, x in enumerate(data):
+            sw.read(x)
+            if j % (k+1) == 0:
+                print(f"x={x}; j={j}; sample={sw.get()}")
+                sample.append(sw.get()[1])
+        # sample += sw.get_sample()
+        sw.clear()
+    print(sample)
+    histogram(sample, k)
+
+
+
+if __name__ == "__main__":
+    n = 100
+    k = 5
+    iter = 100
+    check_correctness(n, k, iter)
+
+
+
 # import random
 # import numpy as np
 # import matplotlib.pyplot as plt
